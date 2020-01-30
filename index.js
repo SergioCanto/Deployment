e217017ec6745d1febd45f1de9d26ed3ae912e92
SergioCanto <enanifero@hotmail.com>
2020-01-30 15:42:05 -0600

    Adding access header

--- index.js
+++ index.js
@@ -11,17 +11,15 @@ let { DATABASE_URL, PORT } = require('./config');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.use(function (req, res, next) {
-
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-
     if (req.method === "OPTIONS") {
-        return res.send(204);
+    return res.send(204);
     }
-
-});
+    next();
+   });
 
 let comentarios = [
     {
